0f91eb9d46841bd42b4393db1508f0060f078561
jonhermsen-UNO <67668642+jonhermsen-UNO@users.noreply.github.com>
2020-10-02 21:23:58 -0500

    feat: add word count to batch process output

--- batch-process-hermsen.sh
+++ batch-process-hermsen.sh
@@ -2,7 +2,9 @@
 for f in "$1"/*; do
   if [ -f "$f" ]; then
     SIZE="$(du -sh "${f}" | cut -f1)"
+    WC="$(wc -w "${f}" | cut -d' ' -f1)"
     echo "Processing $f file..."
-    echo "$SIZE"
+    echo "File size: $SIZE"
+    echo "Word count: $WC"
   fi
 done
